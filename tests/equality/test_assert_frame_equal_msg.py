724f05b9da4a83fa4ea6d2e74fa16fd9e9fcfe78
shreenapatel <shreena88@gmail.com>
2021-10-26 17:37:11 +0100

    updated so that tests requiring numpy and pandas are skipped if numpy or pandas is not installed

--- tests/equality/test_assert_frame_equal_msg.py
+++ tests/equality/test_assert_frame_equal_msg.py
@@ -1,9 +1,18 @@
-import pandas as pd
-import pandas
 import inspect
 import pytest
 
 import test_aide.equality as eh
+
+try:
+
+    import pandas as pd
+    import pandas
+
+    has_pandas = True
+
+except ModuleNotFoundError:
+
+    has_pandas = False
 
 
 def test_arguments():
@@ -64,6 +73,7 @@ def test_arguments():
     ), f"Unexpected keyword arg variable in assert_frame_equal_msg -\n  Expected: {expected_var_keyword_arg}\n  Actual: {var_keyword_arg}"
 
 
+@pytest.mark.skipif(not has_pandas, reason="pandas not installed")
 def test_pandas_assert_frame_called(mocker):
     """Test the call to pandas.testing.assert_frame_equal."""
 
@@ -103,6 +113,7 @@ def test_pandas_assert_frame_called(mocker):
     pd.testing.assert_frame_equal(call_1_expected_pos_args[1], call_1_pos_args[1])
 
 
+@pytest.mark.skipif(not has_pandas, reason="pandas not installed")
 def test_exception_no_print():
     """Test an assert error is raised (with correct info) in case of exception coming from assert_frame_equal and
     print_actual_and_expected is False.
@@ -118,6 +129,7 @@ def test_exception_no_print():
         )
 
 
+@pytest.mark.skipif(not has_pandas, reason="pandas not installed")
 def test_exception_print():
     """Test an assert error is raised (with correct info) in case of exception coming from assert_frame_equal and
     print_actual_and_expected is True.
